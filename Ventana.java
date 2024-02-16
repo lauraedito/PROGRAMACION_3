7183655f07f3027a664aa272d22251921d0e40a5
lauraedito <113810506+lauraedito@users.noreply.github.com>
2024-02-15 17:41:05 -0700

    unidad 2: ejercicio 4

---
+++ Ventana.java
@@ -0,0 +1,15 @@
+//Laura Edith Osorio Maldonado
+
+package ventana;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+	public Ventana() {
+		this.setSize(501, 501);
+	
+	}
+	
+	
+
+}
